6237627e060513678b2ca200ea28936d251b40f0
rejaneklebis <klebis@gmail.com>
2024-08-27 00:47:33 -0300

    Exercício 4

---
+++ Lista2/exerc4resp.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 4</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <main class="container mt-3">
+      <h3>Resposta do Exercício 4</h3>
+      <?php
+          if($_SERVER["REQUEST_METHOD"] == 'POST'){
+              try{
+                  $valor1 = (int) $_POST['valor1'] ?? 0;
+                  $valor2 = (int) $_POST['valor2'] ?? 0;
+                  //+ - * / % ++ -- ** potenciação
+                  if($valor2 == 0){
+                    throw new Exception("Divisão por zero não é permitida.");
+                }
+                  $resultado = $valor1 / $valor2;
+                  echo "<p>Divisão: $resultado </p>";
+
+              }catch(Exception $e){
+                  echo "Erro! ".$e->getMessage(); 
+
+              }
+          }
+
+      ?>
+
+    </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
