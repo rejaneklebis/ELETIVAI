bb082ef356508374cfe2a4006c3fc4e787b1ef82
rejaneklebis <klebis@gmail.com>
2024-08-27 00:11:26 -0300

    Editei com bootstrap

--- Lista2/exerc1.php
+++ Lista2/exerc1.php
@@ -7,25 +7,30 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Exercício 1</h1>
-    <form action="exer1resp.php" method="POST">
-        <div class="row">
-            <div class="col">
-                <label for="valor1" class="form-label">Informe o valor 1</label>
-                <input type="number" class="form-control" name="valor1" id="valor1">
-            </div>
-            <div class="col">
-                <label for="valor2" class="form-label">Informe o valor 2</label>
-                <input type="number" class="form-control" name="valor2" id="valor2">
-            </div>
-        </div>
+    <main class="container mt-3">
+      <h3>Exercício 1</h3>
+      <form action="exer1resp.php" method="POST">
+          <div class="row">
+              <div class="col-4">
+                  <label for="valor1" class="form-label">Informe o valor 1</label>
+                  <input type="number" class="form-control" name="valor1" id="valor1">
+              </div>
+              <div class="col-4">
+                  <label for="valor2" class="form-label">Informe o valor 2</label>
+                  <input type="number" class="form-control" name="valor2" id="valor2">
+              </div>
+          </div>
 
-        <div class="row">
-            <div class="col">
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </div>
-        </div>
+          <div class="row mt-3">
+              <div class="col">
+                  <button type="submit" class="btn btn-primary">Enviar</button>
+              </div>
+          </div>
 
+
+
+    </main>
+    
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
