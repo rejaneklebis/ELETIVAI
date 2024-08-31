a15d1f729220f1cab3f6d9ab86a1b81c1a1197ed
rejaneklebis <klebis@gmail.com>
2024-08-31 10:56:37 -0300

    Exercício 11 - Lista 2

---
+++ Lista2/exerc11.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 11</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <main class="container mt-3">
+    <h3>Exercício 11</h3>
+      <form action="exerc11resp.php" method="POST">
+          <div class="row">
+              <div class="col-4">
+                  <label for="valor1" class="form-label">Insira o raio do círculo:</label>
+                  <input type="number" class="form-control" name="valor1" id="valor1">
+              </div>
+              
+          </div>
+
+          <div class="row mt-3">
+              <div class="col">
+                  <button type="submit" class="btn btn-primary">Enviar</button>
+              </div>
+          </div>
+
+
+
+
+    </main>
+    
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Lista2/exerc11resp.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercício 11</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <main class="container mt-3">
+      <h3>Resposta do Exercício 11</h3>
+      <?php
+          if($_SERVER["REQUEST_METHOD"] == 'POST'){
+              try{
+                  $valor1 = (int) $_POST['valor1'] ?? 0;
+                  
+                  //+ - * / % ++ -- ** potenciação pi.r**2 2pi.r
+                  $resultado = 2 * 3.14 * $valor1;
+                  echo "<p>O perímetro do círculo é: $resultado </p>";
+
+              }catch(Exception $e){
+                  echo "Erro! ".$e->getMessage(); 
+
+              }
+          }
+
+      ?>
+
+    </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
