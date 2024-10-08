27ea81920164aa7a9487054ebf3ff71a14d7b91b
rejaneklebis <klebis@gmail.com>
2024-10-08 20:45:02 -0300

    configuração login e conexão banco de dados

---
+++ projetobd - views/config/bancodedados.php
@@ -0,0 +1,20 @@
+<?php
+
+    $host = "localhost"; 
+    $db = "banco_php";
+    $usuario = "root";
+    $senha = "";
+    $port = "3306"; //para trocar a porta se precisar
+
+    try{
+        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $usuario, $senha);//3 parametros, 1 string de conexão padrão
+        if($pdo){
+            echo "Conexão realizada com sucesso!";
+        }else {
+            echo "Erro ao conectar o banco de dados!";
+        }
+    }catch(Exception $e){
+        echo "Erro: ".$e->getMessage();
+    }
+
+?>
